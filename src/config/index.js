58dd9a79eb056bc598d32fa1e6bf7f9ab683b786
John Goldsmith <johngoldsm1@gmail.com>
2016-06-16 17:59:55 -0700

    Add docs to gitignore

--- src/config/index.js
+++ src/config/index.js
@@ -4,14 +4,7 @@ module.exports = {
    * [blackListedProjectNames description]
    * @type {Array}
    */
-  blackListedProjectNames: [
-    'Active Network, LLC',
-    'Love for Sports',
-    'Verys LLC:Strikers',
-    'Verys LLC:Vacation',
-    'ZeniMax Media Inc.:CDP Admin Fixed Fee',
-    'ZeniMax Media Inc.'
-  ],
+  blackListedProjectNames: [],
 
   /**
    * [notes description]
